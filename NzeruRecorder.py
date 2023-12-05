9890ff761672590e78eaac9c983675bda2e1b55c
Nzeru Creations <93992299+FrancisZoeChituwi@users.noreply.github.com>
2023-12-05 08:59:18 -0800

    webcam feature added

--- NzeruRecorder.py
+++ NzeruRecorder.py
@@ -13,11 +13,18 @@ file_name=f"{time_stamp}.mp4"
 fourcc = cv2.VideoWriter_fourcc("m", "p", "4", "v")
 captured_video = cv2.VideoWriter(file_name, fourcc, 20.0, (width, height))
 
+#added webcam capture code
+webcam = cv2.VideoCapture(0)
+
 while True:
      img = ImageGrab.grab(bbox=(0, 0, width, height))
-     img_np = np.array(img)
+     img_np = np.array(img) 
      img_final = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+     _, frame = webcam.read()
      cv2.imshow("Nzeru Capture", img_final)
+     
+     cv2.imshow("webcam", frame)
+     
      captured_video.write(img_final)
      if cv2.waitKey(10) ==ord("q"):
          break
