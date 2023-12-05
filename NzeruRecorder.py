002f8123e0c9608baf75c19ab8f603f0442ba9b6
Nzeru Creations <93992299+FrancisZoeChituwi@users.noreply.github.com>
2023-12-05 07:14:40 -0800

    Changed the Geometry of the captured window to dynamic from static

--- NzeruRecorder.py
+++ NzeruRecorder.py
@@ -1,11 +1,17 @@
 from PIL import ImageGrab
 import numpy as np
 import cv2
+from win32api import GetSystemMetrics
 
+width = GetSystemMetrics(0)
+height = GetSystemMetrics(1)
+
+print(height, width)
 
 while True:
-     img = ImageGrab.grab(bbox=(0, 0, 1280, 720))
+     img = ImageGrab.grab(bbox=(0, 0, width, height))
      img_np = np.array(img)
-     cv2.imshow("NzeruCapture", img_np)
+     img_final = cv2.cvtColor(img_np, cv2.COLOR_BGR2RGB)
+     cv2.imshow("Nzeru Capture", img_final)
      cv2.waitKey(10)
      
